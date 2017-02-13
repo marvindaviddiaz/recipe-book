b5765e26a26fa840f0a3849ba8be23b843537eba
Marvin Diaz <marvindaviddiaz@hotmail.com>
2017-02-13 06:41:33 -0600

    Template-Driven

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
+import { ShoppingListService } from './shopping-list.service';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
 
-  constructor() { }
+  isAdd: boolean = true;
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
 
-  ngOnInit() {
+  constructor(private sls: ShoppingListService) { }
+
+  ngOnChanges(changes) {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = { name: null, amount: null };
+    } else {
+      this.isAdd = false;
+    }
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    if (this.isAdd) {
+      this.sls.additem(newIngredient);
+      this.onClear();
+    } else {
+      this.sls.editItem(this.item, newIngredient);
+    }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+    this.onClear();
+  }
+
+  onClear() {
+    this.isAdd = true;
+    this.cleared.emit(null);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,4 @@
-import { Ingredient } from '../shared/ingredient'
+import { Ingredient } from '../shared/ingredient';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -14,6 +14,18 @@ export class ShoppingListService {
 
   additems(items: Ingredient[]) {
     Array.prototype.push.apply(this.items, items);
+  }
+
+  additem(item: Ingredient) {
+    this.items.push(item);
+  }
+
+  editItem(oldItem: Ingredient, newItem: Ingredient) {
+    this.items[this.items.indexOf(oldItem)] = newItem;
+  }
+
+  deleteItem(item: Ingredient) {
+    this.items.splice(this.items.indexOf(item), 1);
   }
 
 }
